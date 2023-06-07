23c4ba3424ec2a83796b50855ab289da33de8a97
Laurenz Foglia <lerrrtaste@protonmail.com>
2023-06-07 09:58:45 +0200

    Update autorandr fingerprints

--- hosts/delorean/configuration.nix
+++ hosts/delorean/configuration.nix
@@ -12,9 +12,9 @@
 
 let
   default_fingerprints =  {
-    DisplayPort-1 = "00ffffffffffff001e6dfa7674170300021c0104a55022789fca95a6554ea1260f5054256b807140818081c0a9c0b300d1c08100d1cfcd4600a0a0381f4030203a001e4e3100001a003a801871382d40582c4500132a2100001e000000fd00284b5a5a18010a202020202020000000fc004c4720554c545241574944450a016f020314712309060747100403011f1312830100008c0ad08a20e02d10103e96001e4e31000018295900a0a038274030203a001e4e3100001a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ae§";
+    DisplayPort-1 = "00ffffffffffff004c2d80704e38383007210104b54628783b43a5ae5244b0260f5054bfef80714f810081c081809500a9c0b300010198e200a0a0a0295008403500ba892100001a000000fd003090d6d63b010a202020202020000000fc004c433332473578540a20202020000000ff00484b32573230303739360a202001f6020327f144903f1f042309070783010000e305c0006d1a000002013090000000000000e3060501565e00a0a0a0295030203500ba892100001a6fc200a0a0a0555030203500ba892100001a5a8780a070384d4030203500ba892100001a023a801871382d40582c4500ba892100001e00000000000000000000000000000000d7";
     DisplayPort-2 = "00ffffffffffff004dd9034b0101010101180103804728780a0dc9a05747982712484c2108008180a9c0714fb3000101010101010101023a801871382d40582c4500c48e2100001e011d007251d01e206e285500c48e2100001e000000fc00534f4e592054560a2020202020000000fd00303e0e460f000a202020202020017802032ff0531f101405130420223c3e121603071115020601260d0707150750830f000068030c004000b82d0fe200fb023a80d072382d40102c4580c48e2100001e011d00bc52d01e20b8285540c48e2100001e011d8018711c1620582c2500c48e2100009e011d80d0721c1620102c2580c48e2100009e00000000000000007a";
-    HDMI-A-0 = "00ffffffffffff0009d1417f455400000c1a0103803c22782eeab5a55751a1260c5054a56b80d1c081c081008180a9c0b30001010101023a801871382d40582c450056502100001e000000ff005233473030383937534c300a20000000fd00324c1e5311000a202020202020000000fc0042656e5120524c323735350a200175020322f14f90050403020111121314060715161f2309070765030c00100083010000023a801871382d40582c450056502100001f011d8018711c1620582c250056502100009f011d007251d01e206e28550056502100001e8c0ad08a20e02d10103e960056502100001800000000000000000000000000000000000000000047";
+    HDMI-A-0 = "00ffffffffffff001e6df97674170300021c010380502278eaca95a6554ea1260f5054256b807140818081c0a9c0b300d1c08100d1cfcd4600a0a0381f4030203a001e4e3100001a003a801871382d40582c4500132a2100001e000000fd00284b5a5a18000a202020202020000000fc004c4720554c545241574944450a014c020323f12309070747100403011f13128301000065030c001000681a00000101284b008c0ad08a20e02d10103e96001e4e31000018295900a0a038274030203a001e4e3100001a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a3";
   };
 in
 {                                                                   
@@ -91,81 +91,81 @@ in
         };
       };
 
-      "tv" = {
-        fingerprint = default_fingerprints;
-        config = {
-          HDMI-A-0 = {
-            enable = false;
-          };
-          DisplayPort-1 = {
-            enable = false;
-          };
-          DisplayPort-2 = {
-            enable = true;
-            primary = true;
-            mode = "1920x1080";
-          };
-        };
-      };
+    #   "tv" = {
+    #     fingerprint = default_fingerprints;
+    #     config = {
+    #       HDMI-A-0 = {
+    #         enable = false;
+    #       };
+    #       DisplayPort-1 = {
+    #         enable = false;
+    #       };
+    #       DisplayPort-2 = {
+    #         enable = true;
+    #         primary = true;
+    #         mode = "1920x1080";
+    #       };
+    #     };
+    #   };
 
-      "deck" = {
-        fingerprint = default_fingerprints;
-        config = {
-          HDMI-A-0 = {
-            enable = false;
-          };
-          DisplayPort-1 = {
-            enable = false;
-          };
-          DisplayPort-2 = {
-            enable = true;
-            primary = true;
-            mode = "1280x800";
-          };
-        };
-      };
+    #   "deck" = {
+    #     fingerprint = default_fingerprints;
+    #     config = {
+    #       HDMI-A-0 = {
+    #         enable = false;
+    #       };
+    #       DisplayPort-1 = {
+    #         enable = false;
+    #       };
+    #       DisplayPort-2 = {
+    #         enable = true;
+    #         primary = true;
+    #         mode = "1280x800";
+    #       };
+    #     };
+    #   };
 
 
-      "hdesk" = {
-        fingerprint = default_fingerprints;
-        config = {
-          HDMI-A-0 = {
-            enable = true;
-            primary = true;
-            mode = "1920x1080";
-            position = "0x0";
-          };
-          DisplayPort-1 = {
-            enable = true;
-            primary = false;
-            mode = "2560x1080";
-            position = "1920x0";
-            rate = "75";
-          };
-          DisplayPort-2 = {
-            enable = false;
-          };
-        };
-      };
+    #   "hdesk" = {
+    #     fingerprint = default_fingerprints;
+    #     config = {
+    #       HDMI-A-0 = {
+    #         enable = true;
+    #         primary = true;
+    #         mode = "1920x1080";
+    #         position = "0x0";
+    #       };
+    #       DisplayPort-1 = {
+    #         enable = true;
+    #         primary = false;
+    #         mode = "2560x1080";
+    #         position = "1920x0";
+    #         rate = "75";
+    #       };
+    #       DisplayPort-2 = {
+    #         enable = false;
+    #       };
+    #     };
+    #   };
 
-      "deskgaming" = {
-        fingerprint = default_fingerprints;
-        config = {
-          HDMI-A-0 = {
-            enable = false;
-          };
-          DisplayPort-1 = {
-            enable = true;
-            primary = true;
-            mode = "2560x1080";
-            position = "0x0";
-            rate = "75";
-          };
-          DisplayPort-2 = {
-            enable = false;
-          };
-        };
-      };
+    #   "deskgaming" = {
+    #     fingerprint = default_fingerprints;
+    #     config = {
+    #       HDMI-A-0 = {
+    #         enable = false;
+    #       };
+    #       DisplayPort-1 = {
+    #         enable = true;
+    #         primary = true;
+    #         mode = "2560x1080";
+    #         position = "0x0";
+    #         rate = "75";
+    #       };
+    #       DisplayPort-2 = {
+    #         enable = false;
+    #       };
+    #     };
+    #   };
     };
   };
 
