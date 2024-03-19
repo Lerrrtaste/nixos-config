f382433f6df617603eec72566d20822ac9555206
Laurenz Foglia <lerrrtaste@protonmail.com>
2024-03-19 17:56:24 +0100

    Add doc keys to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -7,9 +7,13 @@ let
   mrfusion = {
     user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpdds88pyZNlu5wo6LmO+HmCpgCYmKL2OPMcEz7eydF";
   };
+  doc = {
+    system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVtwzizfyegqzSAh3snn5EDR9Rg0oP4+NWH1PunRept root@doc";
+    user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvj2hhyxBnyUy2VZjVbW99SCxnkFQwOfYcEmjpyBKQ8";
+  };
 
-  users = [ delorean.user mrfusion.user ];
-  systems = [ delorean.system ]; # system keys are used for decryption when building (user keys just for convenient editing)
+  users = [ delorean.user mrfusion.user doc.user ];
+  systems = [ delorean.system doc.system ]; # system keys are used for decryption when building (user keys just for convenient editing)
 in
 {
   "wg-quick-conf.age".publicKeys = systems ++ users;
