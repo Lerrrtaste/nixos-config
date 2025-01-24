ebff39d30172ab4bada5a5a25b12cd394b4e317d
Laurenz Foglia <lerrrtaste@protonmail.com>
2025-01-24 08:35:53 +0100

    Update secrets; Add k1-master

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,13 +2,15 @@ let
   mrfusion = {
     user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBYLFR6Wy8//3A/qlM+aGpRx4/maUeJxpMeRreAyAHm";
     system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpdds88pyZNlu5wo6LmO+HmCpgCYmKL2OPMcEz7eydF";
+    #k1-master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBUxY2FUei2jteN/4Ar/cXuqoktEnxt+XNFcNxrGSBfI k1-master:openpgp:0xFFF9E7FD"; # for non-replacable secrets; to restore from backup
   };
   doc = {
     system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEATs+bgG/UqESkqRpgBS83V6/7Lsk73z9/GZkmsMzHA";
     user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+SaeOJHqoqOVNAKcvPBP1s//OwdqKnhpwBBZprHqOA";
+    #k1-master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBUxY2FUei2jteN/4Ar/cXuqoktEnxt+XNFcNxrGSBfI k1-master:openpgp:0xFFF9E7FD"; # for non-replacable secrets; to restore from backup
   };
 
-  users = [ mrfusion.user doc.user ];
+  users = [ mrfusion.user doc.user k1-master ];
   systems = [ mrfusion.system doc.system ]; # system keys are used for decryption when building (user keys just for convenient editing)
 in
 {
@@ -21,6 +23,10 @@ in
 
   "ts-auth-doc.age".publicKeys = [doc.system doc.user];
 
-  "cb-pw.age".publicKeys = [doc.system doc.user];
-  "cb-creds-doc.age".publicKeys = [doc.system doc.user];
+  "cb-pw.age".publicKeys = [doc.system doc.user ];
+  "cb-creds-doc.age".publicKeys = [ doc.system doc.user ];
+
+  "c1-sync-id.age".publicKeys = systems ++ users;
+
+  "st-doc-key.age".publicKeys = doc;
 }
