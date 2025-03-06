78ad1be1948240fbcf2163f7bc353c38aaecd43d
Laurenz Foglia <lerrrtaste@protonmail.com>
2025-03-06 07:29:47 +0100

    [carbon] start host config

---
+++ hosts/carbon/configuration.nix
@@ -0,0 +1,19 @@
+# wip hardened minimal nixos
+# - only important packages
+# - no unfree code
+# - vms for unsafer stuff
+# - minimal permissions for user
+# - seperate admin user with sudo access?
+#
+# services
+# - dedicated user for each one
+# - systemd.exec man page (RootEphemeral, ProtectHome/System/Kernel*/
+#
+# ideas
+# - rolling release channel?
+# - wayland?
+# - fs impermanence?
+#
+# reads
+# - https://documentation.suse.com/smart/security/html/systemd-securing/index.html
+# - https://www.redhat.com/en/blog/systemd-secure-services
